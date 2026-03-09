a530eef6efb3f3b5cd4cfe9730f2b0e203c6ade3
Jonatas-01 <jonatasmc02@gmail.com>
2026-03-09 12:59:22 +0000

    style: Setup var styles and light/dark mode styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 
 export default function Home() {
   return (
-    <div className="">Home Page</div>
+    <div className="">
+      <h1 className="text-4xl">Welcome to the Home Page</h1>
+    </div>
   );
 }
